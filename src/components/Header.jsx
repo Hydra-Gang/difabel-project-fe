44c5ed8675473ff06d8170ef4faf1ec77ec5182f
unknown <fajarmh2@gmail.com>
2022-03-18 20:38:06 +0700

    Add template navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,31 @@
 import React from "react";
+import { Navbar, Button, Container, Nav } from "react-bootstrap";
 
 function Header() {
-    return (
-        <div>
-            <h1>Hay gan</h1>
-        </div>
-    );
-}
+    return(
+        <Navbar collapseOnSelect expand="lg" className="color-nav" variant="dark">
+        <Container>
+        <Navbar.Brand href="#home">Logo</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+            <Nav.Link href="#features">Dashboard</Nav.Link>
+            <Nav.Link href="#map">Map</Nav.Link>
+            <Nav.Link href="#forum">Forum</Nav.Link>
+            <Nav.Link href="#report">Report</Nav.Link>
+            </Nav>
+            <Nav>
+            <Nav.Link href="#deets">
+                <Button>Sign Up</Button>
+            </Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+                <Button>Login</Button>
+            </Nav.Link>
+            </Nav>
+        </Navbar.Collapse>
+        </Container>
+        </Navbar>
+    )
+};
 
 export default Header;
