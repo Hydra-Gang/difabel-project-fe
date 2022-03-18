503dfc2228e3113d45bc592c438061fef81d0431
unknown <fajarmh2@gmail.com>
2022-03-18 21:53:07 +0700

    Add navbar in Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,61 @@
 import React from "react";
 import { Navbar, Button, Container, Nav } from "react-bootstrap";
 
+const image = "assets/Logo.png";
+const whiteColor = "white";
+const greenColor = "#01634B"
+
 function Header() {
     return(
-        <Navbar collapseOnSelect expand="lg" className="color-nav" variant="dark">
-        <Container>
-        <Navbar.Brand href="#home">Logo</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto">
-            <Nav.Link href="#features">Dashboard</Nav.Link>
-            <Nav.Link href="#map">Map</Nav.Link>
-            <Nav.Link href="#forum">Forum</Nav.Link>
-            <Nav.Link href="#report">Report</Nav.Link>
-            </Nav>
-            <Nav>
-            <Nav.Link href="#deets">
-                <Button>Sign Up</Button>
-            </Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-                <Button>Login</Button>
-            </Nav.Link>
-            </Nav>
-        </Navbar.Collapse>
-        </Container>
-        </Navbar>
+        <>
+            <style type="text/css">
+                    {`
+                    .btn-green {
+                        background-color: ${greenColor};
+                        color: ${whiteColor};
+                        font-weight: 700;
+                    }
+
+                    .btn-white {
+                        background-color: ${whiteColor};
+                        color: ${greenColor};
+                        font-weight: 700;
+                    }
+
+                    .btn-xxl {
+                    padding: 1rem 1.5rem;
+                    font-size: 1.5rem;
+                    }
+
+                    `}
+            </style>
+
+            <Navbar collapseOnSelect expand="lg">
+            <Container>
+            <Navbar.Brand href="#home">
+                <img src={image} alt="Logo" />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="me-auto">
+                <Nav.Link href="#features" className="bold margin-left">Dashboard</Nav.Link>
+                <Nav.Link href="#map" className="bold margin-left">Map</Nav.Link>
+                <Nav.Link href="#forum" className="bold margin-left">Forum</Nav.Link>
+                <Nav.Link href="#report" className="bold margin-left">Report</Nav.Link>
+                </Nav>
+                <Nav>
+                <Nav.Link href="#signup">
+                    <Button variant="white">Sign Up</Button>
+                </Nav.Link>
+                <Nav.Link eventKey={2} href="#memes">
+                    <Button variant="green">Login</Button>{' '}
+                </Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+            </Container>
+            </Navbar>
+        </>
+        
     )
 };
 
