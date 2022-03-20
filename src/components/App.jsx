35edb3e6950ea7bad508a0ea8120a978377830de
Julian_A_Wardana <julian.wardana@binus.ac.id>
2022-03-20 16:08:53 +0700

    add article page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import React from "react"
 import Content from "./Content";
 import Header from "./Header";
+import ArticlePage from "./ArticlePage/ArticlePage";
 
 function App() {
     return (
         <>
             <Header />
-            <Content />
+            {/* <Content /> */}
+            <ArticlePage />
         </>
     );
 }
