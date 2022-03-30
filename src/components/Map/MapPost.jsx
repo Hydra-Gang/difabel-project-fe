2e6055f85ab1267e2dc654d34018dbcb0e23309e
unknown <fajarmh2@gmail.com>
2022-03-30 15:31:50 +0700

    Create POST map using AXIOS, style not yet

--- src/components/Map/MapPost.jsx
+++ src/components/Map/MapPost.jsx
@@ -1,17 +1,61 @@
-import { Button } from 'react-bootstrap';
-import React, { useEffect, useState } from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-// import difabelsData from './difabel-location.json';
 import styled from '@emotion/styled';
-import PropTypes from 'prop-types';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
 
+const Heading = styled.h1`
+    color: #01634B;
+    text-align: left;
+    font-size: 40px;
+    font-weight: 700;
+    text-align: center;
+    margin-top: 5%;
+`;
 
 function MapPost() {
+    const [name, setName] = useState('');
+    const [type, setType] = useState('');
+    const [address, setAddress] = useState('');
+    const [latitude, setLatitude] = useState('');
+    const [longitude, setLongitude] = useState('');
+
+    function postData(e) {
+        e.preventDefault();
+        axios.post('http://localhost:5000/v1/map/add', {
+            name,
+            type,
+            address,
+            latitude,
+            longitude
+        }).then((res) => {
+            console.log('Sucessfully added');
+            setName('');
+            setType('');
+            setAddress('');
+            setLatitude('');
+            setLongitude('');
+        }).error((err) => {
+            console.err(err);
+        });
+
+    }
+
     return (
         <div className='container'>
-            MapPost
+            <Heading>Add Difabel Location</Heading>
+            <a href="https://www.google.com/maps/">Cek your longitude, latitude, address in here!</a>
+            <form>
+                <label>Name</label> <br/>
+                <input type="text" value={name} onChange={(e) => setName(e.target.value)}></input> <br/>
+                <label>Type</label> <br/>
+                <input type="text" value={type} onChange={(e) => setType(e.target.value)}></input> <br/>
+                <label>Address</label> <br/>
+                <input type="text" value={address} onChange={(e) => setAddress(e.target.value)}></input> <br/>
+                <label>Latitude</label> <br/>
+                <input type="text" value={latitude} onChange={(e) => setLatitude(e.target.value)}></input> <br/>
+                <label>Longitude</label> <br/>
+                <input type="text" value={longitude} onChange={(e) => setLongitude(e.target.value)}></input> <br/>
+                <button type="submit" onClick={postData}>Add Map</button>
+            </form>
         </div>
     );
 }
