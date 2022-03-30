327408a7e38af1f5cec5d47c5608e7a943d7d2f6
leonardolim78 <leonardolim178@gmail.com>
2022-03-30 23:29:37 +0700

    * Add schema for donate validation * Add request donation to backend

--- src/components/DonationPage/DonationForm.jsx
+++ src/components/DonationPage/DonationForm.jsx
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 import { Button, Form, Image } from 'react-bootstrap';
 import { FaDonate, FaHandHoldingHeart, FaLaugh, FaUser } from 'react-icons/fa';
 import styled from 'styled-components';
+import axios from '../../axios-instance';
+import { schema } from '../../validations/donate-validation';
 import DonationFormInput from './DonationFormInput';
 
 const Box = styled.div`
@@ -42,10 +44,46 @@ const DonationForm = () => {
         money: ''
     });
 
+    const [errors, setErrors] = useState({});
+    const [backendError, setBackendError] = useState('');
     const [success, setSuccess] = useState(false);
 
-    const validateForm = () => {
-        setSuccess(true);
+    const validateForm = async (e) => {
+        const result = schema.validate(data, { abortEarly: false });
+        const { error } = result;
+
+        e.preventDefault();
+
+        if (error) {
+            const errorData = {};
+
+            for (const err of error.details) {
+                const name = err.path[0];
+                const message = err.message;
+                errorData[name] = message;
+            }
+
+            setErrors(errorData);
+        } else {
+            try {
+                await axios.post('/donations/add', data);
+
+                setData({
+                    donator: '',
+                    money: ''
+                });
+
+                setErrors({
+                    donator: '',
+                    money: ''
+                });
+
+                setSuccess(true);
+            } catch (err) {
+                const errorMessage = err.response.data.message;
+                setBackendError(errorMessage);
+            }
+        }
     };
 
     return (
@@ -54,9 +92,9 @@ const DonationForm = () => {
                 <Form className="w-100">
                     <h6 className="text-center mb-3 fw-normal" style={{ color: '#01634B' }}><FaHandHoldingHeart /> Your donation means a lot to us</h6>
                     <DonationImage as={Image} src="assets/donate.png" className="d-block m-auto" alt="Donate"></DonationImage>
-                    {/* {backendError && <div className="alert alert-success w-100 py-2 mb-2 m-auto float-lg-start text-start" role="alert">{backendError}</div>} */}
+                    {backendError && <div className="alert alert-success w-100 py-2 mb-2 m-auto float-lg-start text-start" role="alert">{backendError}</div>}
 
-                    <FormInputContext.Provider value={[data, setData]}>
+                    <FormInputContext.Provider value={[data, setData, errors]}>
                         <DonationFormInput propKey="donator" propName="Name" iconName={<FaUser />} />
                         <DonationFormInput propKey="money" propName="Donation Amount" iconName={<FaDonate />} />
                         <DonateButton as={Button} type="submit" className="w-100 mb-3 border-0" onClick={validateForm}>Donate</DonateButton>
